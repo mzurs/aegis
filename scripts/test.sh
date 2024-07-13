319bbd1b320f22c9dd24a610fbffd16060be0bb6
Muhammad Zohaib <zohaib10092001@gmail.com>
2024-07-13 15:22:55 +0500

    scripts added

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,41 @@
 #!/usr/bin/bash
 
 test_type=$1
+test_canister_name=$2
 
 if [[ $test_type == "unit" ]]; then
+
+    if [[ $test_canister_name == "accounts" ]]; then
+
+        echo "Running Accounts Uint Tests"
+
+        jest -c ./jest.config.ts --testPathPattern='tests/unit/accounts'
+
+    elif [[ $test_canister_name == "insurance" ]]; then
+
+        echo "Running Insurance Uint Tests"
+
+        jest --testPathPattern='tests/unit/insurance'
+
+    fi
+
+elif [[ $test_type == "integration" ]]; then
+
+    if [[ $test_canister_name == "accounts" ]]; then
+
+        echo "Running Accounts Integration Tests"
+
+    elif [[ $test_canister_name == "insurance" ]]; then
+
+        echo "Running Insurance Integration Tests"
+
+        jest --testPathPattern='tests/integration/insurance/1_insurance.spec.ts'
+
+        jest --testPathPattern='tests/integration/insurance/2_insurance.spec.ts'
+
+    fi
+
+elif [[ $test_type == "all" ]]; then
 
     echo "Running Accounts Uint Tests"
 
@@ -11,8 +44,6 @@ if [[ $test_type == "unit" ]]; then
     echo "Running Insurance Uint Tests"
 
     jest --testPathPattern='tests/unit/insurance'
-
-elif [[ $test_type == "integration" ]]; then
 
     echo "Running Insurance Integration Tests"
 
