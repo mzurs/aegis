f584e13106a2414d29bf580f20b7f58053038e0f
Muhammad Zohaib <zohaib10092001@gmail.com>
2025-01-24 00:44:19 +0500

    fix

--- tests/integration/main/main.spec.ts
+++ tests/integration/main/main.spec.ts
@@ -47,7 +47,12 @@ describe("======================================Main Canister Integration Testin
   let mainCanisterActor: Actor<_MAIN>;
 
   beforeAll(async () => {
-    pic = await PocketIc.create(process.env.PIC_URL);
+    pic = await PocketIc.create(process.env.PIC_URL, {
+      nns: true,
+      fiduciary: true,
+      bitcoin: true,
+    });
+
     await pic.resetTime();
     minter = createIdentityFromSeed("minter");
     user1 = createIdentityFromSeed("user1");
